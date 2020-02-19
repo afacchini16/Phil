e18760c65a2286144a033b6b484dbdbe14b8f85f
Tony Facchini <afacchini16@gmail.com>
2020-02-18 16:48:25 -0800

    Server Folder

---
+++ server/index.js
@@ -0,0 +1,10 @@
+/** @file imports the express app from app.js and starts the server on port 9000 */
+'use strict';
+
+const app = require('./app');
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
